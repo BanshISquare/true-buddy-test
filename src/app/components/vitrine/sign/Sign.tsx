81938c70c559b4b79ea9d688dc0741bee70029d2
JGrenet <jgrenet@infinitesquare.com>
2019-08-03 12:00:23 +0200

    remove prettier and change debugging config

--- src/app/components/vitrine/sign/Sign.tsx
+++ src/app/components/vitrine/sign/Sign.tsx
@@ -1,51 +1,46 @@
+import classNames from "classnames";
 import * as React from "react";
-import { useState } from "react"
-import classNames from 'classnames';
+import { useState } from "react";
 import "./Sign.scss";
 
+const Sign: React.FunctionComponent<any> = props => {
+	return (
+		<div className="loginContainer">
+			<div className="componentContainer">
+				<SignIn />
+				<SignUp />
+			</div>
+		</div>
+	);
+};
 
-const Sign: React.FunctionComponent<any> = (props) => {
+const SignIn: React.FunctionComponent<any> = props => {
+	const [isContentOpened, setIsContentOpened] = useState(false);
 
-    return (
-        <div className="loginContainer">
-            <div className="componentContainer">
-                <SignIn></SignIn>
-                <SignUp></SignUp>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className="signInContainer">
+			<div
+				className={classNames("signContentContainer", {
+					"signContentContainer--closed": !isContentOpened,
+					"signContentContainer--opened": isContentOpened
+				})}
+			/>
+			<button
+				className={classNames("btn btn--large login", {
+					"btn--alone": !isContentOpened,
+					"btn--not-alone": isContentOpened
+				})}
+				onClick={() => {
+					setIsContentOpened(!isContentOpened);
+				}}>
+				Se connecter
+			</button>
+		</div>
+	);
+};
 
-const SignIn: React.FunctionComponent<any> = (props) => {
-
-    const [isContentOpened, setIsContentOpened] = useState(false);
-
-    return (
-        <div className="signInContainer">
-            <div className={classNames('signContentContainer', {
-                    'signContentContainer--opened': isContentOpened,
-                    'signContentContainer--closed': !isContentOpened,
-                })}
-            >
-            </div>
-            <button className={classNames("btn btn--large login", {
-                    'btn--not-alone': isContentOpened,
-                    'btn--alone': !isContentOpened,
-                })}
-                onClick={() => {setIsContentOpened(!isContentOpened)}}
-            >
-                Se connecter
-            </button>
-        </div>
-    );
-}
-
-const SignUp: React.FunctionComponent<any> = (props) => {
-
-    return (
-        <div className="signUpContainer">
-        </div>
-    );
-}
+const SignUp: React.FunctionComponent<any> = props => {
+	return <div className="signUpContainer" />;
+};
 
 export default Sign;
