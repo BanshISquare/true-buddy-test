9d0b856b8c0d1a3cdeba8b41291e3a6481995d7c
JGrenet <jgrenet@infinitesquare.com>
2019-08-05 14:52:56 +0200

    fix eslint errors and change typescript module resolution strategy

--- src/app/components/vitrine/sign/Sign.tsx
+++ src/app/components/vitrine/sign/Sign.tsx
@@ -1,47 +1,42 @@
-import classNames from "classnames";
-import * as React from "react";
-import { useState } from "react";
-import "./Sign.scss";
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import './Sign.scss';
 
-const Sign: React.FunctionComponent<any> = props => {
-	return (
-		<div className="loginContainer">
-			<div className="componentContainer">
-				<SignIn />
-				<SignUp />
-			</div>
+const Sign: React.FunctionComponent<any> = (): JSX.Element => (
+	<div className="loginContainer">
+		<div className="componentContainer">
+			<SignIn />
+			<SignUp />
 		</div>
-	);
-};
+	</div>
+);
 
-const SignIn: React.FunctionComponent<any> = props => {
+const SignIn: React.FunctionComponent<any> = (props): JSX.Element => {
 	const [isContentOpened, setIsContentOpened] = useState(false);
 
 	return (
 		<div className="signInContainer">
-			<div
-				className={classNames("signContentContainer", {
-					"signContentContainer--closed": !isContentOpened,
-					"signContentContainer--opened": isContentOpened
-				})}
+			<div className={classNames('signContentContainer', {
+				'signContentContainer--closed': !isContentOpened,
+				'signContentContainer--opened': isContentOpened,
+			})}
 			/>
 			<button
-				className={classNames("btn btn--large login", {
-					"btn--alone": !isContentOpened,
-					"btn--not-alone": isContentOpened
+				className={classNames('btn btn--large login', {
+					'btn--alone': !isContentOpened,
+					'btn--not-alone': isContentOpened,
 				})}
-				onClick={() => {
+				onClick={(): void => {
 					setIsContentOpened(!isContentOpened);
-				}}>
+				}}
+				type="button"
+			>
 				Se connecter
 			</button>
 		</div>
 	);
 };
 
-const SignUp: React.FunctionComponent<any> = props => {
-
-	return <div className="signUpContainer" />;
-};
+const SignUp: React.FunctionComponent<any> = (props): JSX.Element => <div className="signUpContainer" />;
 
 export default Sign;
