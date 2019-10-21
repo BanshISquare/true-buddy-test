7e709fcdc770654d2b647e8ec55c8a51ee8bce39
JGrenet <jgrenet@infinitesquare.com>
2019-10-21 15:07:44 +0200

    feat(connected-content): adding connected content to connected-shell with a basic style

--- src/app/components/connected/ConnectedShell.tsx
+++ src/app/components/connected/ConnectedShell.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Header from './header/Header';
+import ContentShell from './content-shell/ContentShell';
 
 const ConnectedShell: React.FunctionComponent<{}> = (): JSX.Element => (
 	<>
 		<Header />
+		<ContentShell />
 	</>
 );
 
---
+++ src/app/components/connected/content-shell/ContentShell.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ContentShell: React.FunctionComponent<{}> = (): JSX.Element => (
+	<div className="content-shell" />
+);
+
+export default ContentShell;
