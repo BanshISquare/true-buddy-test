2dbd34458d3273bcf9f0df672b619b9126f2ba5e
JGrenet <jgrenet@infinitesquare.com>
2019-10-21 15:05:07 +0200

    feat(connected-header): adding basic style to connected header

--- src/app/components/connected/header/Header.tsx
+++ src/app/components/connected/header/Header.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
 const Header: React.FunctionComponent<{}> = (): JSX.Element => (
-    <div className="connected-header" />
+	<div className="connected-header">
+		<div className="container-fluid connected-header_content" />
+	</div>
 );
 
 export default Header;
