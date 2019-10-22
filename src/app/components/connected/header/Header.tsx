c8676cba33a024631851a0db2ebbde7b03571470
JGrenet <jgrenet@infinitesquare.com>
2019-10-22 11:59:04 +0200

    feat(connected-header): Basic architecture of connected header with components to do

--- src/app/components/connected/header/Header.tsx
+++ src/app/components/connected/header/Header.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
 
+const LocaleManager = (): JSX.Element => <></>;
+const HeaderSocials = (): JSX.Element => <></>;
+const UserProfile = (): JSX.Element => <></>;
+const HeaderNavigation = (): JSX.Element => <></>;
+const HeaderSearch = (): JSX.Element => <></>;
+
 const Header: React.FunctionComponent<{}> = (): JSX.Element => (
 	<div className="connected-header">
-		<div className="container-fluid connected-header_content" />
+		<div className="container-fluid connected-header_content-container">
+			<div className="site-logo" />
+			<div className="header-content">
+				<div className="header-content_sub">
+					<>
+						<LocaleManager />
+						<HeaderSocials />
+					</>
+					<UserProfile />
+				</div>
+				<div className="header-content_sub">
+					<HeaderNavigation />
+					<HeaderSearch />
+				</div>
+			</div>
+		</div>
 	</div>
 );
 
